7ede7b718344b634df2a6a7b232912112bbc3903
lim1017 <lim1017@hotmail.com>
2020-01-15 17:48:24 +0000

    added name

---
+++ client.js
@@ -0,0 +1,33 @@
+const net = require('net');
+
+/**
+ * Establishes connection with the game server
+ */
+const connect = function() {
+  const conn = net.createConnection({ 
+    host: '172.46.2.204',
+    port: 50541
+  });
+
+  conn.on('connect', () =
+  > {
+    // console.log('Connected!');
+
+    conn.write("Name: BOB");  //sends to server
+  });
+
+  conn.on('data', (data) => {
+    console.log('Server says: ', data);
+  });
+  
+
+  // interpret incoming data as text
+  conn.setEncoding('utf8'); 
+
+  return conn;
+}
+
+console.log('Connecting ...');
+connect();
+
+module.export=connect
